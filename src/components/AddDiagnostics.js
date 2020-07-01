184c3fa5e81444ba27af6c5b83c13d876bd68c4c
Sai Krishna <49117132+NamedSaiKrishna@users.noreply.github.com>
2020-07-01 22:27:30 +0530

    Redux

--- src/components/AddDiagnostics.js
+++ src/components/AddDiagnostics.js
@@ -6,7 +6,6 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 
--- src/components/ViewAllPatients.js
+++ src/components/ViewAllPatients.js
@@ -4,7 +4,6 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
 import Paper from "@material-ui/core/Paper";
 import TableHead from "@material-ui/core/TableHead";
 import { makeStyles } from "@material-ui/core/styles";
@@ -13,9 +12,6 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableRow from "@material-ui/core/TableRow";
-import EditIcon from "@material-ui/icons/Edit";
-import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
-import ReceiptIcon from "@material-ui/icons/Receipt";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
